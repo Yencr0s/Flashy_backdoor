702d893b24b416d4665cfd51d23af5043465be63
Roberto Riaño <87758528+Yencr0s@users.noreply.github.com>
2025-09-16 19:09:14 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -6,7 +6,7 @@ request = portal.context.makeRequestRSpec()
 node = request.RawPC("node0")
 
 # Pin the A100 box type (Wisconsin)
-node.hardware_type = "d8545"  # HGX A100 (4x40GB) at UW site
+node.hardware_type = "d840"  # HGX A100 (4x40GB) at UW site
 
 # Stock Ubuntu 22 image (fast boot)
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
